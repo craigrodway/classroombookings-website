a953211dedbb778cd591e93f7d7bfeec9bb330ec
Craig A Rodway <craig.rodway@gmail.com>
2019-01-01 22:33:02 +0000

    Added favicons

--- public_html/site/templates/_main.php
+++ public_html/site/templates/_main.php
@@ -24,6 +24,7 @@ $cssUrl = $cssUrl.'?id='.filemtime($cssPath);
 		gtag('config', 'UA-3041463-3');
 		</script>
 		<?php endif; ?>
+
 		<title><?= $titleTag ?></title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,6 +34,16 @@ $cssUrl = $cssUrl.'?id='.filemtime($cssPath);
 		<meta name="description" content="<?= region('description') ?>">
 		<meta name="author" content="Craig A Rodway">
 		<link href="<?= $cssUrl ?>" rel="stylesheet" type="text/css">
+		<link rel="apple-touch-icon" sizes="180x180" href="/brand/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/brand/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/brand/favicon-16x16.png">
+		<link rel="manifest" href="/brand/site.webmanifest">
+		<link rel="mask-icon" href="/brand/safari-pinned-tab.svg" color="#ff6400">
+		<link rel="shortcut icon" href="/brand/favicon.ico">
+		<meta name="msapplication-TileColor" content="#ff6400">
+		<meta name="msapplication-config" content="/brand/browserconfig.xml">
+		<meta name="theme-color" content="#ff6400">
+
 	</head>
 
 	<body>
