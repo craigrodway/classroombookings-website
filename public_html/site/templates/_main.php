c8b3ebdaa3e677e33a88e0b0c0d35399034fccea
Craig A Rodway <craig.rodway@gmail.com>
2019-02-10 18:12:37 +0000

    Add fathom analytics

--- public_html/site/templates/_main.php
+++ public_html/site/templates/_main.php
@@ -81,6 +81,20 @@ $cssUrl = $cssUrl.'?id='.filemtime($cssPath);
 
 		</div>
 
+		<script>
+		(function(f, a, t, h, o, m){
+			a[h]=a[h]||function(){
+				(a[h].q=a[h].q||[]).push(arguments)
+			};
+			o=f.createElement('script'),
+			m=f.getElementsByTagName('script')[0];
+			o.async=1; o.src=t; o.id='fathom-script';
+			m.parentNode.insertBefore(o,m)
+		})(document, window, '//fathom.onhover.co.uk/tracker.js', 'fathom');
+		fathom('set', 'siteId', 'WSHIS');
+		fathom('trackPageview');
+		</script>
+
 	</body>
 
 </html>
