1615546be856e0d183e8d33553ee341e03ef6ad3
Craig A Rodway <craig.rodway@gmail.com>
2019-10-30 11:36:06 +0000

    Column classes for testimonials.

--- public_html/site/templates/partials/testimonials.php
+++ public_html/site/templates/partials/testimonials.php
@@ -6,7 +6,7 @@ namespace ProcessWire;
 
 	<?php foreach ($testimonials as $testimonial): ?>
 
-	<div class="column col-xs-12 col-lg-6 col-xl-3">
+	<div class="column col-xs-12 col-sm-12 col-lg-6 col-xl-3 col-3">
 		<blockquote class="quote-testimonial">
 			<p><?= $testimonial->summary ?></p>
 			<cite><?= $testimonial->title ?></cite>
